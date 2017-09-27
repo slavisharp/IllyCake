eb2e509a0567427901e8b5e0a3611a7b340525bb
Slavi Tsvetanov <slavi.cvetanov@yarillo.com>
2017-09-27 14:51:09 +0300

    CSS/JS setup
    Setup gulp watch
    Setup js bundles and minification
    Setup less and css minification
    Remove bootstrap 4 and popper

--- IllyCake.Web/gulpfile.js
+++ IllyCake.Web/gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require("gulp"),
     fs = require("fs"),
+    watch = require('gulp-watch'),
     less = require("gulp-less");
 
 gulp.task("less", function () {
     return gulp.src('Styles/main.less')
         .pipe(less())
         .pipe(gulp.dest('wwwroot/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('Styles/**/*.less', ['less']);
 });
